2298b91d96ef6305de3b2dda4d840d661392fff5
goldwater-stock <ruisu9un@gmail.com>
2026-01-17 10:37:06 +0800

    Update app.js

--- js/app.js
+++ js/app.js
@@ -239,8 +239,19 @@ function getValue(stock, key) {
   if (!el) return '';
 
   let t = el.textContent.trim();
-  if (key === 'price' || key === 'change') {
-    return parseFloat(t.replace('%', '').replace('+', '')) || 0;
-  }
-  return t;
+ if (key === 'price') {
+  return parseFloat(
+    t.replace('$', '')
+     .replace(',', '')
+     .trim()
+  ) || 0;
 }
+
+if (key === 'change') {
+  return parseFloat(
+    t.replace('%', '')
+     .replace('+', '')
+     .trim()
+  ) || 0;
+}
+
